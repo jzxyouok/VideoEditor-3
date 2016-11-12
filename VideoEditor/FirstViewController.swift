28ef3a08142ec812a81ea57e2f81724653ff4ae6
yanovskaya <yanovskaya1997@list.ru>
2016-11-12 15:58:26 -0800

    ...

--- VideoEditor/FirstViewController.swift
+++ VideoEditor/FirstViewController.swift
@@ -14,7 +14,38 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var dataView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textFieldButton.layer.cornerRadius = 2
+        textFieldButton.layer.borderWidth = 1
+        textFieldButton.layer.borderColor = UIColor.gray.cgColor
+        
+        let myImages = ["1.jpeg", "2.jpg", "3.jpg", "4.jpg"]
+        let imageWidth: CGFloat = 160
+        let imageHeight: CGFloat = 100
+        var xPosition:CGFloat = 0
+        var scrollViewContentSize:CGFloat=0
+        
+        for index in 0 ..< myImages.count
+        {
+            let myImage:UIImage = UIImage(named: myImages[index])!
+            let myImageView: UIImageView = UIImageView()
+            myImageView.image = myImage
+            myImageView.contentMode = UIViewContentMode.scaleAspectFit
+            
+            myImageView.frame.size.width = imageWidth
+            myImageView.frame.size.height = imageHeight
+            myImageView.frame.origin.y = 10
+            myImageView.center = self.view.center
+            myImageView.frame.origin.x = xPosition
+            
+            dataView.addSubview(myImageView)
+            let spacer: CGFloat = 10
+            
+            xPosition+=imageWidth + spacer
+            scrollViewContentSize+=imageWidth + spacer
+            
+            dataView.contentSize = CGSize(width: scrollViewContentSize, height: imageHeight)
+        }
     }
 
     override func didReceiveMemoryWarning() {
