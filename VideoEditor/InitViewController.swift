73c1f4ef31c63065f744cb546d6a65d09e165d7c
Maria Korovina <eyentei@MacBook-Air-Maria.local>
2016-11-16 21:57:25 +0300

    added onboarding

---
+++ VideoEditor/InitViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InitViewController.swift
+//  VideoEditor
+//
+//  Created by Мария Коровина on 16.11.16.
+//  Copyright © 2016 Елена Яновская. All rights reserved.
+//
+
+import UIKit
+
+class InitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore {
+            performSegue(withIdentifier: "toMainScreen", sender: self)
+        } else {
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+        }*/
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
