061de019fa5ae7b8ad8aa2754a9e9a9f2f0afa41
rGradeStd <gulakovivan97@gmail.com>
2016-11-17 21:26:01 +0300

    Onboarding

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'VideoEditor' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'paper-onboarding', '~> 2.0.1'
+  # Pods for VideoEditor
+
+  target 'VideoEditorTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'VideoEditorUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- VideoEditor/InitViewController.swift
+++ VideoEditor/InitViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import paper_onboarding
 
-class InitViewController: UIViewController {
+class InitViewController: UIViewController,PaperOnboardingDataSource,PaperOnboardingDelegate {
+    /**
+     Tells the delegate that the paperOnbording start scrolling.
+     
+     - parameter index: An curretn index item
+     */
+    public func onboardingWillTransitonToIndex(_ index: Int) {
+        //
+    }
+
+    func onboardingDidTransitonToIndex(_ index: Int){
+//
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-        if launchedBefore {
-            performSegue(withIdentifier: "toMainScreen", sender: self)
-        } else {
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
-        }*/
+
+        let onboarding = PaperOnboarding(itemsCount: 3)
+        onboarding.dataSource = self
+        onboarding.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(onboarding)
+
+        // add constraints
+        for attribute: NSLayoutAttribute in [.left, .right, .top, .bottom] {
+            let constraint = NSLayoutConstraint(item: onboarding,
+                                                attribute: attribute,
+                                                relatedBy: .equal,
+                                                toItem: view,
+                                                attribute: attribute,
+                                                multiplier: 1,
+                                                constant: 0)
+            view.addConstraint(constraint)
+        }
     }
-    
+
+    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
+        return [
+            ("1", "Title", "Description text", "IconName1", UIColor.blue, UIColor.red, UIColor.black, UIFont.systemFont(ofSize: 13), UIFont.systemFont(ofSize: 13)),
+            ("2", "Title", "Description text", "IconName1",  UIColor.black, UIColor.black, UIColor.black, UIFont.systemFont(ofSize: 13), UIFont.systemFont(ofSize: 13)),
+            ("3", "Title", "Description text", "IconName1",  UIColor.black, UIColor.black, UIColor.black, UIFont.systemFont(ofSize: 13),UIFont.systemFont(ofSize: 13))
+            ][index]
+    }
+
+    func onboardingConfigurationItem(_ item: OnboardingContentViewItem, index: Int) {
+       //
+    }
+
+    func onboardingItemsCount() -> Int {
+        return 3
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
