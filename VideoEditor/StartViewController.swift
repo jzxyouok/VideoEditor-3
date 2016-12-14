90af3ec5e4c5ec1088229cfaad65c05a094c7a04
yanovskaya <yanovskaya1997@list.ru>
2016-12-14 12:19:53 -0800

    perform segue using buttons

--- VideoEditor/StartViewController.swift
+++ VideoEditor/StartViewController.swift
@@ -17,47 +17,52 @@ class StartViewController: UIViewController {
         UIImage(named: "2.jpg")!,
         UIImage(named: "3.jpg")!,
         UIImage(named: "4.png")!]
+    
 
+    
 
     var myImageView: [ScrollImageView] = []
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let button = UIButton (frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+        
         button.backgroundColor = UIColor.green
         button.setTitle("+", for: .normal)
         button.addTarget(self, action: #selector(addToScroll(button:)), for: .touchDown)
         view.addSubview(button)
 
-
-        textFieldButton.layer.cornerRadius = 2
+                textFieldButton.layer.cornerRadius = 2
         textFieldButton.layer.borderWidth = 1
         textFieldButton.layer.borderColor = UIColor.gray.cgColor
 
 
         let imageWidth: CGFloat = 160
         let imageHeight: CGFloat = 100
-        var xPosition:CGFloat = +0
-        var scrollViewContentSize:CGFloat=0
-
-
-        for index in 0 ..< myImages.count
+        var xPosition: CGFloat = 0
+        var scrollViewContentSize: CGFloat=0
+//HEEEELP!!!!
+        
+                for index in 0 ..< myImages.count
         {
+            let b1 = UIButton (frame: CGRect(x: xPosition, y: 0, width: imageWidth, height: imageHeight))
+        b1.addTarget(self, action: #selector(toPrevEdit(b1:b5:)), for: .touchDown)
 
             let image: UIImage? = myImages[index]
             myImageView.append(ScrollImageView(frame: CGRect(x: xPosition, y: 0, width: imageWidth, height: imageHeight)))
-
+            
             myImageView[index].image = image
 
             self.myScrollView.addSubview(myImageView[index])
-
+self.myScrollView.addSubview(b1)
+            
+            
             myImageView[index].isUserInteractionEnabled = true
 
             let spacer: CGFloat = 20
 
 
-            xPosition+=imageWidth + spacer
+            xPosition += imageWidth + spacer
             scrollViewContentSize+=imageWidth + spacer
 
             myScrollView.contentSize = CGSize(width: scrollViewContentSize - spacer, height: imageHeight)
@@ -67,6 +72,10 @@ class StartViewController: UIViewController {
 
     var scrollViewContentSize : CGFloat = 720
     var xPosition : CGFloat = 540
+    //?????
+    func toPrevEdit (b1 : UIButton, b5: UIButton) {
+        performSegue(withIdentifier: "toPreviousEdit", sender: self)
+    }
     func addToScroll (button: UIButton) {
         
         let imageWidth : CGFloat = 160
@@ -77,6 +86,7 @@ class StartViewController: UIViewController {
         scrollViewContentSize += imageWidth + spacer
         xPosition += imageWidth + spacer
         
+        
         let imageViewToAdd : UIImageView = ScrollImageView(frame: CGRect(x: xPosition, y: 0, width: imageWidth, height: imageHeight))
 
         imageViewToAdd.image = myImages[0]
@@ -85,7 +95,11 @@ class StartViewController: UIViewController {
 
         self.myScrollView.addSubview(imageViewToAdd)
         self.myScrollView.contentSize = CGSize(width: scrollViewContentSize - spacer, height: imageHeight)
+        let b5 = UIButton (frame: CGRect(x: xPosition, y: 0, width: imageWidth, height: imageHeight))
+        b5.addTarget(self, action: #selector(toPrevEdit(b1:b5:)), for: .touchDown)
+        
 
+        self.myScrollView.addSubview(b5)
         print("it tapped")
         print(myImages[0], myImages[1], myImages.count)
         
