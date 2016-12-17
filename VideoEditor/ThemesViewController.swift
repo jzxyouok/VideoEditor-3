7e2f086f7e4954609d913986bae8e12c641910d9
yanovskaya <yanovskaya1997@list.ru>
2016-12-17 10:27:47 -0800

    improved themes view

--- VideoEditor/ThemesViewController.swift
+++ VideoEditor/ThemesViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ThemesViewController: UIViewController {
+class ThemesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var previewImage: UIImageView!
     @IBOutlet weak var allThemeExamples: UITableView!
@@ -27,16 +27,32 @@ class ThemesViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCellTheme
+        
+        return cell
+    }
 
 }
+
+class CustomCellTheme: UITableViewCell {
+   
+    @IBOutlet weak var textTheme: UITextView!
+    
+    
+    @IBOutlet weak var picture: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
--- VideoEditor/ViewAllController.swift
+++ VideoEditor/ViewAllController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewAllController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
