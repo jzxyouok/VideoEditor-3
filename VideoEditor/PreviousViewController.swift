97d094dc36e2c8d9258c402abafb85192e9f7f45
yanovskaya <yanovskaya1997@list.ru>
2016-12-17 06:52:58 -0800

    +new swift file

---
+++ VideoEditor/PreviousViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PreviousViewController.swift
+//  VideoEditor
+//
+//  Created by Елена Яновская on 14/12/16.
+//  Copyright © 2016 Елена Яновская. All rights reserved.
+//
+import UIKit
+
+class PreviousViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
